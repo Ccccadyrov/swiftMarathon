f6d817d5ad8f5fe1e3c2b26e40721015e1bd4b78
Mikhael <msheplyakovq@gmail.com>
2023-05-02 04:47:45 +1000

    add neles

---
+++ S17.playground/Contents.swift
@@ -0,0 +1,61 @@
+//subscript
+
+let array = ["a", "b", "c", "d"]
+array[0]
+
+//сабскрипт - обращение через квадратные скобки по индексу (к экземпляру) может принимат инт, стринг и т.д.
+//применяются к классам структурам енумам могут иметь несколько значений
+
+struct Family {
+    var father = "Father"
+    var mother = "Mother"
+    var kids = ["Kid1", "Kid2", "Kid3"]
+    
+    var count:Int {
+        get {
+            return 2+kids.count
+        }
+    }
+    
+    subscript(index:Int) -> String? {
+        get { //сеттеры так-же работают
+            switch(index) {
+            case 0: return father
+            case 1: return mother
+            case 2..<(2+kids.count): return kids[index-2]
+            default: return nil
+            }
+        }
+    }
+}
+
+var family = Family()
+family.father
+family.count
+family.kids[0]
+family[0]
+family[2]//работаем через сабскрипт
+//family[0]="DAD"  сработало бы если бы прописал сеттер
+
+
+struct Field {
+    
+    var dict = [String: String] ()
+    
+    subscript(column: String, row: Int) -> String? {
+        get {
+            return dict[column+String(row)]
+        }
+        set {
+            dict[column+String(row)] = newValue
+        }
+    }
+}
+
+var field = Field()
+field["a", 5]
+field["a", 5] = "X"
+field["a", 5]
+
+//шахматная доска, когда по сабскрипту обращаемся к ячейке - узнаем ее цвет (лучше из енума), если вне поля - то нил
+//крестики нолики, создать поле, ячейка содержит пусто крестик или нолик + возможность распечатать, по сабскрипту обращаемся к ячейке и ставим значение
