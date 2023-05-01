75c72238a29fe4cb76caacfe69319f6295b85eaa
Mikhael <msheplyakovq@gmail.com>
2023-05-02 03:26:19 +1000

    Add homework

--- S16HW.playground/Contents.swift
+++ S16HW.playground/Contents.swift
@@ -1,14 +1,46 @@
+class Player {
+    var x: Int
+    var y: Int
+    var image = "😎"
+    init (x:Int, y:Int) {
+        self.x = x
+        self.y = y
+    }
+    func moveRight () {
+        self.y+=1
+    }
+    func moveDown () {
+        self.x+=1
+    }
+}
 
-var array = [Int]()
+var player = Player(x: 1, y: 2)
+var z = player.image
+player.x
+player.y
+var clearSpace = "⬜️"
 
+
+var array = [String]()
 func painter() {
     for i in 1...9 {
         array = []
         for j in 1...9 {
-            array.append(j)
+            if (i==player.x && j==player.y) {
+                array.append(z)
+            } else {
+                array.append(clearSpace)
+            }
+            
         }
         print (array)
     }
+    print("ОТБИВКА")
 }
 painter()
-
+player.moveRight()
+painter()
+player.moveRight()
+painter()
+player.moveDown()
+painter()
